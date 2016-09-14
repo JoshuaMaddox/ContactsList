fe589621360c7d869f30e8313ea954fd10fe9146
Joshua Maddox <HomeFolder@Joshuas-MacBook-Pro.local>
2016-09-14 12:55:33 -0700

    created file

---
+++ app.js
@@ -0,0 +1,27 @@
+// Add contacts to the list.
+
+//Get the value of the form field input
+//Append the variable contianing the input value to the table
+
+// Render the list neatly.
+// Be able to update the data of any contact in your list.
+// Be able to delete contacts.
+
+// Extra Features:
+
+// Render your create / update forms in a modal (popup). 
+// Bootstrap has a pretty nifty modal included!
+
+// Add images to your contacts. These can be image urls (strings). 
+
+// Be sure to limit the rendered size of the images so they aren't too huge.
+
+// Be able to sort your contacts by any of the fields by clicking the field headers.
+
+// Have a custom search bar that will filter your contacts as you type. 
+
+// This will reduce your visible contacts to those which have any field that 
+// matches a part of your input.
+
+// Categorize your contacts into groups (friend, family, colleague, etc.), 
+// and allow to filter by group.
