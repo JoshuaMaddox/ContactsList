accaede46fed75023d0cfdb7b206ff4483333ab6
Joshua Maddox <HomeFolder@Joshuas-MacBook-Pro.local>
2016-09-14 15:55:48 -0700

    prepared basic functions

--- app.js
+++ app.js
@@ -1,11 +1,78 @@
 // Add contacts to the list. name, phone number, address, email
+let $fullName, $mobileNumber, $emailAddy, $contAddress, $cateGory, $contactsTable; 
+
+//Append the variable contianing the input value to the table
+$(() => {
+  $fullName = $('#fullName'); 
+  $mobileNumber = $('#mobileNumber');  
+  $emailAddy = $('#emailAddy'); 
+  $contAddress = $('#contAddress'); 
+  $cateGory = $('#cateGory');
+  $contactsTable = $('#contactsTable')
+  $('#contactFormEntry').submit(storeContact);
+});
+
+function storeContact(event) {
+  event.preventDefault()
+  let fullName = $fullName.val(); 
+  let mobileNumber = $mobileNumber.val();  
+  let emailAddy = $emailAddy.val(); 
+  let contAddress = $contAddress.val(); 
+  let cateGory = $cateGory.val();
+  let contactsObj = {
+    fullname: fullName,
+    mobilenumber: mobileNumber,
+    email: emailAddy,
+    address: contAddress,
+    category: cateGory
+  } 
+  console.log("I am fullName: " + fullName);
+  console.log("I am mobileNumber: " + mobileNumber);
+  console.log("I am emailAddy: " + emailAddy);
+  console.log("I am contAddress: " + contAddress);
+  console.log("I am cateGory: " + cateGory);
+}
+
+
+  let $row = createNewRow(fullName, mobileNumber, emailAddy, contAddress, cateGory);
+
+  $contactsTable.append($row);
+
+  function createNewRow() {
+    let $row = $('#templateTableRow').clone();
+  }
+
+
+
+
+// function init() {
+//   //   var contactsObj = {};
+//   $('.submitContact').click(storeInputVariables);
+// }
+ 
+
+
 
 //Get the value of the form field input
-//Append the variable contianing the input value to the table
+// function storeContact() {
+  // fullName = $('#fullName').val();
+  // mobileNumber = $('#mobileNumber').val();
+  // emailAddy = $('#emailAddy').val();
+  // contAddress = $('#contAddress').val();
+  // cateGory = $('#cateGory').val();
+  // contactsObj = {
+  //   fullname: fullName,
+  //   mobilenumber: mobileNumber,
+  //   email: emailAddy,
+  //   address: contAddress,
+  //   category: cateGory
+//   }
+  
+// }
 
-$(document).ready(function(){
-  alert("connected");
-});
+
+
+
 
 // Render the list neatly.
 // Be able to update the data of any contact in your list.
